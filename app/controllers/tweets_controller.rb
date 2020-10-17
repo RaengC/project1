cc62c9134f7523a14776e372b740d279d178dc69
RaengC <raengc@yahoo.com>
2020-10-17 15:13:25 +1100

    Edit tweet added, checked works, fixed click through from indext to indvidual tweet

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -16,7 +16,15 @@ class TweetsController < ApplicationController
     redirect_to tweet
   end
 
+  def edit
+    @tweet = Tweet.find params[:id]
+  end
 
+  def update
+    tweet = Tweet.find params[:id]
+    tweet.update tweet_params
+    redirect_to tweet
+  end
 
   private
   def tweet_params
