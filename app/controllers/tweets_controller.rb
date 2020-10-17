01de4c7b5e010ea53f76447dbb276721b3deee20
RaengC <raengc@yahoo.com>
2020-10-17 12:53:34 +1100

    tweet, create, show and nav links added and working

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,8 +3,23 @@ class TweetsController < ApplicationController
       @tweets = Tweet.all
   end
 
+  def show
+    @tweet = Tweet.find params[:id]
+  end
+
   def new
     @tweet = Tweet.new
   end
 
+  def create
+    tweet = Tweet.create tweet_params
+    redirect_to tweet
+  end
+
+
+
+  private
+  def tweet_params
+     params.require(:tweet).permit(:user_id, :content, :image)
+  end
 end
