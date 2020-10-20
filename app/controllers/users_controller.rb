fa2eeed9ab9370cb4b4e766b1ad80d9f9c7ce165
RaengC <raengc@yahoo.com>
2020-10-20 12:12:58 +1100

    users tweets displaying on profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
-  before_action :signed_in_user,
-                only: [:index, :edit, :update, :destroy, :following, :followers]
+
 
   def show
     @user = User.find params[:id]
