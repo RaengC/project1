af46ae4c3d2ce352787e99277cbec690a5933037
RaengC <raengc@yahoo.com>
2020-10-21 09:59:52 +1100

    delete comment working

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,10 +4,17 @@ class CommentsController < ApplicationController
       @tweet = Tweet.find(params[:tweet_id])
       @comment = @tweet.comments.create(comment_params)
       redirect_to tweet_path(@tweet)
-    end
+  end
 
-    private
-      def comment_params
-        params.require(:comment).permit(:commenter, :body)
-      end
+  def destroy
+    @tweet = Tweet.find(params[:tweet_id])
+    @comment = @tweet.comments.find(params[:id])
+    @comment.destroy
+    redirect_to tweet_path(@tweet)
+  end
+
+  private
+    def comment_params
+      params.require(:comment).permit(:commenter, :body)
+    end
 end
