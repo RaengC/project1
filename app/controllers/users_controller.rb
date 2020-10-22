87da36eb15c4a4d86ab287ff72fe7234bb682736
RaengC <raengc@yahoo.com>
2020-10-22 19:50:20 +1100

    fixed bug in creating new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,15 +10,16 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new user_params
-    if params[:file].present?
-      req = Cloudinary::Uploader.upload(params[:file])
-      user.image = req["public_id"]
-      user.save
+
       if @user.save
       session[:user_id] = @user.id
       redirect_to root_path
         else
           render :new
+          if params[:file].present?
+            req = Cloudinary::Uploader.upload(params[:file])
+            user.image = req["public_id"]
+            user.save
         end
       end
   end
